9d53feee747283d5c37aaeed29d882b12a9ef8a9
Wayne Tasaki <waynetasaki@gmail.com>
2022-02-24 20:07:07 -0800

    added comments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -4,3 +4,11 @@ const reverseString = function(string) {
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// string.split("") - splits a string into an array of individual characters (example: "hello" turns into ["h", "e", "l", "l", "o"])
+
+// string.reverse() - reverses an array
+
+// string.join("") - joins the array of characters into a string
+
+// https://www.samanthaming.com/tidbits/83-4-ways-to-convert-string-to-character-array/ and https://sebhastian.com/javascript-array-string/
