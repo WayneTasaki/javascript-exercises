bc746ea13e45453047b7e03b940c4a414e781c8a
Wayne Tasaki <waynetasaki@gmail.com>
2022-03-30 14:25:44 -0700

    exercises completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+// npm test calculator.spec.js
+
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr == 0) {
+    return 0
+  } else {
+	let sum = 0
+  return arr.reduce((a, b) => a + b, sum)
+}
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let product = 1;
+  return arr.reduce((a, b) => a * b, product);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1)
+    return 1;
+  for (var i = a - 1; i >= 1; i--) {
+    a *= i;
+}
+return a;
 };
 
 // Do not edit below this line
