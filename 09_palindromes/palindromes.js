939d1c78c5103fb4c8d82d61d2615b943a98a4b1
Wayne Tasaki <waynetasaki@gmail.com>
2022-03-30 14:50:25 -0700

    exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+// npm test palindromes.spec.js
 
+const palindromes = function (a) {
+let regex = /[^a-z]/gi;
+let aOnlyLetters = a.replace(regex, "").toLowerCase();
+let aReversed = [...a.replace(regex, "")].reverse().join('').toLowerCase()
+if (aOnlyLetters === aReversed) {
+  return true
+} else {
+  return false
+}
 };
 
 // Do not edit below this line
