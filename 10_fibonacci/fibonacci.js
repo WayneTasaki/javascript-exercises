0a419c5ef2fe7300c91e56ba2afc02d548dabc61
Wayne Tasaki <waynetasaki@gmail.com>
2022-03-30 15:11:49 -0700

    exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+// npm test fibonacci.spec.js
 
+var fibonacci_series = function (n) 
+{
+  if (n==1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
 };
+
+const fibonacci = function(n) {
+  if (n < 0) {
+    return 'OOPS'
+  } else {
+  let num = fibonacci_series(n)
+  return num.pop()
+  }
+};
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
