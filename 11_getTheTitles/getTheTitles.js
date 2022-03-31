f59dd9303219370f1a4b4b47a2684e4c718ddf60
Wayne Tasaki <waynetasaki@gmail.com>
2022-03-30 19:22:04 -0700

    exercise completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
 
+const getTheTitles = function(a) {
+  let titles = a.map(a => 
+    a.title)
+  return titles
 };
 
+// npm test getTheTitles.spec.js
 // Do not edit below this line
 module.exports = getTheTitles;
